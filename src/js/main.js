6f4d27f75fc7d89d447961ed71dbd4e27b5183e8
aidfromdeagland <aidfromdeagland@gmail.com>
2020-08-24 17:40:18 +0300

    feat: mobile layout ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 console.log('INTERACTED');
+
+const questionsBlock = document.querySelector('.faq__questions')
+const questions = questionsBlock.querySelectorAll('.question');
+questionsBlock.addEventListener('click', (evt) => {
+  if (evt.target.closest('.question__toggler')) {
+    const questionItem = evt.target.closest('.question');
+    if (questionItem.classList.contains('question_active')) {
+      questionItem.classList.remove('question_active');
+    } else {
+      Array.prototype.forEach.call(questions, (question) => {
+        if (question.classList.contains('question_active')) {
+          question.classList.remove('question_active');
+        }
+      });
+      questionItem.classList.add('question_active');
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devtool: 'source-map',
   devServer:{
@@ -35,11 +36,30 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(otf|ttf|woff(2))$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts',
+              publicPath: 'fonts',
+            }
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/img', to: 'images' },
+        { from: 'src/fonts', to: 'fonts' },
+      ],
     }),
   ],
 };
